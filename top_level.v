3c782d127167dc8f3a0f060f840de3728b248198
Hyun Yi <hy00un__@naver.com>
2023-12-27 05:49:01 +0900

    -

--- top_level.v
+++ top_level.v
@@ -24,23 +24,77 @@ module top_level(
   // input wire [15:0] answer, 
   output wire lcd_e, lcd_rs, lcd_rw,
   output wire [7:0] lcd_data
+//   output wire	[3:0] Output1,
+// output wire	[3:0] Output2
 );
 
   parameter  [15:0] answer = 16'h1234;
   wire [3:0] strike;  // strike
   wire [3:0] ball;    // ball
 
-  wire [3:0] one_bin, two_bin, three_bin, four_bin, five_bin, six_bin, seven_bin, eight_bin, nine_bin, zero_bin;
+wire zero_to_nine;
 
-  decimal_to_binary decimal_to_binary_inst(
-  .zero(zero), .one(one), .two(two), .three(three), .four(four),
-  .five(five), .six(six), .seven(seven), .eight(eight), .nine(nine),
-  .zero_bin(zero_bin), .one_bin(one_bin), .two_bin(two_bin), .three_bin(three_bin),
-  .four_bin(four_bin), .five_bin(five_bin), .six_bin(six_bin), .seven_bin(seven_bin), 
-  .eight_bin(eight_bin), .nine_bin(nine_bin)
-);
+assign zero_to_nine = zero | one | two | three | four | five | six | seven | eight | nine;
 
-  wire [15:0] guess = {one_bin, two_bin, three_bin, four_bin};
+wire	[3:0] reg1;
+wire	[3:0] reg2;
+wire	[3:0] reg3;
+wire	[3:0] reg4;
+
+wire	[3:0] Din;
+d2b	b2v_inst6(
+	.d0(zero),
+	.d1(one),
+	.d2(two),
+	.d3(three),
+	.d4(four),
+	.d5(five),
+	.d6(six),
+	.d7(seven),
+	.d8(eight),
+	.d9(nine),
+	.b0(Din[0]),
+	.b1(Din[1]),
+	.b2(Din[2]),
+	.b3(Din[3]));
+
+// trigger
+wire trigger_signal;
+trigger	b2v_inst2(
+	.Din(zero_to_nine),
+	.CLK(clk),
+	.rst_n(~rst),
+	.Dout(trigger_signal));
+
+four_bit_register	b2v_inst(
+	.Ce(trigger_signal),
+	.CLK(clk),
+	.RST_N(~rst),
+	.Din(Din),
+	.Dout(reg1));
+
+four_bit_register	b2v_inst4123(
+	.Ce(trigger_signal),
+	.CLK(clk),
+	.RST_N(~rst),
+	.Din(reg1),
+	.Dout(reg2));
+
+four_bit_register	b2v_inst424(
+	.Ce(trigger_signal),
+	.CLK(clk),
+	.RST_N(~rst),
+	.Din(reg2),
+	.Dout(reg3));
+
+four_bit_register	b2v_inst554(
+	.Ce(trigger_signal),
+	.CLK(clk),
+	.RST_N(~rst),
+	.Din(reg3),
+	.Dout(reg4));
+
+  wire [15:0] guess = {reg4, reg3, reg2, reg1};
   
   wire [7:0] lcd_data_internal;
 
